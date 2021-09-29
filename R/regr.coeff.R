fa1d40d4cca1acc09b28cf9bd67e7cd98cc8af26
yupingikub <yupingikub@gmail.com>
2021-09-29 20:07:56 +0200

    Add new functions

---
+++ R/regr.coeff.R
@@ -0,0 +1,33 @@
+#' @title Calculates regression coefficients
+#' @description Calculates regression coefficients of all label_ids for a 
+#' control group.
+#' @details Input output from icv.label.count() which is the data frame 
+#' contains ICVs of all participants in the control group to calculate 
+#' regression coefficients of all label_ids. 
+#' @param icvdf A data frame contains ICV, meanicv and volumes for all label_ids
+#' of all participants in the control group. Uses icv.label.count() to generate 
+#' input data. 
+#' @return Regression coefficients for all label_ids and mean ICV for the 
+#' control group. 
+#' @export
+
+regr.coeff <- function(icvdf) {
+  meanicv <- icvdf%>%
+    dplyr::group_by('pid')%>%
+    dplyr::summarize(icvmean=base::mean(icv_mm3))%>%
+    dplyr::pull(icvmean)
+  
+  vols <- icvdf%>%
+    dplyr::group_by(label_id) %>%
+    dplyr::select(pid, p.class, time_points, rnum, 
+           label_id, icv_mm3, meanicv, vol_mm3)%>%
+    dplyr::do(broom::tidy(lm(vol_mm3 ~ icv_mm3, data = .)))%>%
+    dplyr::select(label_id, term, estimate)%>%
+    tidyr::pivot_wider(.,names_from = term, values_from = estimate)%>%
+    left_join(icvdf, by='label_id')%>%
+    dplyr::select(label_id, k=icv_mm3.x)%>%
+    dplyr::mutate(meanicv=meanicv)%>%
+    stats::na.omit()
+  return(vols)
+}
+
---
+++ R/regr.vol.R
@@ -0,0 +1,37 @@
+#' @title Calculates normalized volumes
+#' @description Calculates normalized volumes of all label ids for all 
+#' individuals in the input data frame. 
+#' @details Input two data frames which the first data frame contains regression
+#' coefficients and the second contains ICV of all individuals,
+#' the third contains all label_id volumes of all individuals. 
+#' @param regrdf A data frame including regression coefficients for label_id. 
+#' We recommend that you use regr.coeff() to generate input data.
+#' @param icvdf label_id volume data frame must contain ten columns of which 
+#' first column has column name pid with character format, the second column 
+#' has column name p.class with character format, the third column has column 
+#' name time_points with integer format, the fourth column has column name 
+#' label_id with integer format, the fifth column has column name rnum with 
+#' integer format, the sixth column has column name class.id with 
+#' integer format, the seventh column has column label_name with character 
+#' format, eighth column has column name vol_mm3 with numeric format and the 
+#' ninth column has column name icv_mm3 with numeric format. We recommend that 
+#' you use icv.label.count() to generate input data. 
+#' @return Returns a data frame including normalized volumes and none normalized
+#' volumes for all individuals. 
+#' @export
+regr.vol <- function(regrdf, icvdf) {
+  df <- icvdf%>%
+    dplyr::full_join(regrdf, by='label_id')%>%
+    dplyr::mutate(icvdev=icv_mm3-meanicv)%>%
+    dplyr::mutate(vol_mm3=vol_mm3-k*icvdev, norm='regr_ICV')%>%
+    dplyr::select(pid, time_points, rnum, label_id, label_name, 
+                  class.name, vol_mm3, norm)%>%
+    base::rbind(., icvdf %>%
+                  dplyr::select(pid, time_points, rnum, label_id, label_name, 
+                         class.name, vol_mm3)%>%
+                  dplyr::mutate(norm='none'))%>%
+    dplyr::select(pid, time_points, rnum, label_id, class.name,
+           R575.uniq.name=label_name, norm, vol_mm3)%>%
+    tidyr::pivot_wider(names_from = c("pid", "time_points"), values_from = "vol_mm3")
+  return(df)
+}
